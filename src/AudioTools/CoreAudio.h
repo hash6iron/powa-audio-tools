acad7bb9a686d6d6c27af5e334420bd407b99744
hash6iron <hash6iron@gmail.com>
2025-08-29 04:20:17 +0200

    modified:   src/AudioTools/CoreAudio.h

--- src/AudioTools/CoreAudio.h
+++ src/AudioTools/CoreAudio.h
@@ -12,25 +12,36 @@
 #include "AudioTools/CoreAudio/ResampleStream.h"
 #include "AudioTools/CoreAudio/ResampleStreamT.h"
 #include "AudioTools/CoreAudio/StreamCopy.h"
+
 #ifdef FULL_LIBRARY
     #include "AudioTools/CoreAudio/MusicalNotes.h"
 #endif
+
 #include "AudioTools/CoreAudio/Fade.h"
 #include "AudioTools/CoreAudio/Pipeline.h"
 #include "AudioTools/CoreAudio/AudioPlayer.h"
 #include "AudioTools/CoreAudio/AudioTimer.h"
 #include "AudioTools/CoreAudio/AudioFilter.h"
 #include "AudioTools/CoreAudio/I2SStream.h"
+
 #ifdef FULL_LIBRARY
     #include "AudioTools/CoreAudio/AudioPWM.h"
 #endif
+
 #include "AudioTools/CoreAudio/AnalogAudioStream.h"
+
 #ifdef FULL_LIBRARY
     #include "AudioTools/CoreAudio/AudioEffects.h"
 #endif
+
 #include "AudioTools/CoreAudio/AudioMetaData.h"
+
 #ifdef FULL_LIBRARY
     #include "AudioTools/CoreAudio/AudioHttp.h"
 #endif
+
 #include "AudioTools/CoreAudio/FrequencyDetector.h"
-#include "AudioTools/CoreAudio/GoerzelStream.h"
+
+#ifdef FULL_LIBRARY
+    #include "AudioTools/CoreAudio/GoerzelStream.h"
+#endif
